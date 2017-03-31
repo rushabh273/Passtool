d83243f906e70018e2b4b4cc3a7cb6a5d009fc31
Timizki <paananen.t@gmail.com>
2017-03-31 21:18:46 +0300

    lenght is now given as parameter

--- generatePass.sh
+++ generatePass.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+LENGTH=
 
 function generate {
 	while true
 	do  
 		read SECRET
   		PASS=`echo -n $SECRET | sha256sum | base64` 
-		echo ${PASS:0:76} 
+		if [ "$LENGTH" == "" ] ; then
+			LENGTH=${#PASS}	
+		fi
+		echo ${PASS:0:$LENGTH} 
 	done 
 }
 function clear {
